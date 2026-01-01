0d0dc4cd9424864e083285e33a20c610722b482b
mentorDemo <gurtberly@gmail.com>
2026-01-01 23:38:16 +0300

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+    "./app/global.css",
+  ],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#030014"
+      },
+    },
+  },
+  plugins: [],
+}
